ef9daa214f1023fffef695eb0114ad5e0618166f
Adriano <adriano.pinheiro@alphasquad.cx>
2023-03-03 12:16:50 -0300

    first commit

---
+++ teste.js
@@ -0,0 +1,24 @@
+function teste() {
+    return 'ola'
+}
+
+
+function teste2() {
+    return 'ola'
+}
+
+function teste3() {
+    return 'ola'
+}
+
+
+function teste4() {
+    return 'ola'
+}
+
+module.exports = {
+    teste: teste,
+    teste2: teste2,
+    teste3: teste3,
+    teste4: teste4
+}
